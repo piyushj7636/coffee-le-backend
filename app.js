5c439168acc137d97cec820d628041f809f66f31
piyushj7636 <158646485+piyushj7636@users.noreply.github.com>
2025-09-06 12:31:23 +0530

    added google service account data as env variable

--- app.js
+++ app.js
@@ -29,7 +29,8 @@ app.use(cookieParser());
 connectDB()
 
 const require = createRequire(import.meta.url);
-const serviceAccount = require('./config/serviceAccountKey.json');
+// const serviceAccount = require('./config/serviceAccountKey.json');
+const serviceAccount = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
